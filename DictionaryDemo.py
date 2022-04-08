c0b6a0649374c4ea5b8c7d1e6c1ae63641b41108
Uma Shanker <umashanker.testing@gmail.com>
2022-04-08 16:55:37 +0530

    message

--- DictionaryDemo.py
+++ DictionaryDemo.py
@@ -3,9 +3,9 @@ from Basics import c
 
 dic = {1: "abc", "a": 12, "c": "xyz"}
 print(dic)
-print(dic["a"])     # print the value against key c
+print(dic["a"])  # print the value against key c
 
-dict={}
-dict["key1"]="KeyValue1"
-dict["key2"]="KeyValue2"
+dict = {}
+dict["key1"] = "KeyValue1"
+dict["key2"] = "KeyValue2"
 print(dict)

b5b40ee7d16242729b4f826d6a7a56b4ad5baf9f
Uma Shanker <umashanker.testing@gmail.com>
2022-04-12 19:03:53 +0530

    modified

---
+++ ContantData.py
@@ -0,0 +1,2 @@
+CONST_VALUE = 10
+print(CONST_VALUE) # it is read as contant but we can modify it in python
---
+++ StribfFunctions'/StrinDemo.py
@@ -0,0 +1,18 @@
+email = "umashanker.testing@gmail.com"
+msg = """
+        This is line 1
+        This is line 2
+        This is line 3
+"""
+
+print(msg)
+print(email)
+
+arr = email.split(".")
+print(arr[0])
+
+dobilequote = "D'Suiza"
+singlquote = ' Hello. " World '
+
+print(dobilequote)
+print(singlquote)
